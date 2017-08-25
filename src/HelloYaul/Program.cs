854cd0f83e00e6473d75191b50b94de9a55b461b
Mendel Agterberg <magterberg@mirabeau.nl>
2017-08-25 09:42:24 +0200

    Added placeholder project for CI

---
+++ src/HelloYaul/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloYaul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
