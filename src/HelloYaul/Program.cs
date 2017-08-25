d83b4610b7812cd384ec1037655d43d8d4d48618
Mendel Agterberg <magterberg@mirabeau.nl>
2017-08-25 10:28:06 +0200

    Removed compiler error

--- src/HelloYaul/Program.cs
+++ src/HelloYaul/Program.cs
@@ -6,7 +6,7 @@ namespace HelloYaul
     {
         static void Main(string[] args)
         {
-            Casdonsole.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
